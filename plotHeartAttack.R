c3113615d4ff7212f74cdd2e7be8ef8ece855c75
Dreamastiy <editor@xakep.ru>
2015-01-24 21:51:40 +0300

    PlotHeartAttack
    Hist of heart attack deaths in US hospital

---
+++ plotHeartAttack.R
@@ -0,0 +1,7 @@
+setwd('C:/Users/Dmi/Documents/R/Data/rprogdataProgAssignment3data')
+outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+head(outcome)
+
+outcome[, 11] <- as.numeric(outcome[, 11])
+## You may get a warning about NAs being introduced; that is okay
+hist(outcome[, 11])
